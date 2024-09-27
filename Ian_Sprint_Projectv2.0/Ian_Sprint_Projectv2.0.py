6c172cd6d903a704b931b6b8e9e8a02f5b5a1475
IanSu18 <144109031+IanSu18@users.noreply.github.com>
2024-09-27 08:11:37 +0000

    Add try-exception handler

--- Ian_Sprint_Projectv2.0/Ian_Sprint_Projectv2.0.py
+++ Ian_Sprint_Projectv2.0/Ian_Sprint_Projectv2.0.py
@@ -1,2 +1,10 @@
-do = input('Please enter your age?: ')
-print(f'{dog}?! that is a great age to be!')
+try:
+    input('What is ur age?: ') 
+    print()  
+    dog = input('Please enter your age?: ')
+    print(f'{dog}?! that is a great age to be!')
+
+    dog = int(input('What is ur age?: '))
+    print(f'{dog}?! That is a great age to be!')
+except ValueError:
+    ptint('Error: That is not a valid number')
